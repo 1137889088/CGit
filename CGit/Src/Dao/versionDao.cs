70073ab6e3df1c2c956842033506e78551a1c8c3
chen <1137889088@qq.com>
2017-05-14 17:41:45 +0800

    未同步文件添加

---
+++ CGit/Src/Dao/versionDao.cs
@@ -0,0 +1,61 @@
+﻿using CGit.Dao;
+using CGit.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGit.Src.Dao
+{
+    public class VersionDao: BaseDao<MVersion>
+    {
+        public static String dataBase = "CGit";
+
+        /// <summary>
+        /// 将数据库记录封装成MVersion对象的方法
+        /// </summary>
+        /// <param name="reader">SqlDataReader类</param>
+        /// <returns>封装好的一个User对象</returns>
+        public MVersion versionHandler(SqlDataReader reader)
+        {
+            MVersion version = new MVersion();
+            version.version_id = reader.GetInt32(0);
+            version.repository_id = reader.GetInt32(1);
+            version.version_identity = reader.GetString(2);
+            version.version_date = reader.GetString(3);
+            return version;
+        }
+
+        /// <summary>
+        /// 根据repositoryId查询所有版本
+        /// </summary>
+        /// <returns>根据repositoryId查找所有version，如果未找到返回null</returns>
+        public List<MVersion> findAllVersionByRepositoryId(int repositoryId)
+        {
+            String sql = "SELECT * from CGit.version where repositoryId= @repositoryId";
+            List<SqlParameter> sqlParameters = new List<SqlParameter>();
+            sqlParameters.Add(new SqlParameter("@repositoryId", repositoryId));
+            List<MVersion> list = query(dataBase, sql, sqlParameters, versionHandler);
+            return list;
+        }
+
+        /// <summary>
+        /// 新建版本
+        /// </summary>
+        /// <param name="repository">需要保存的仓库</param>
+        /// <returns>影响的行</returns>
+        /// 
+        public int save(MVersion version)
+        {
+            String sql = "INSERT INTO CGit.repository (repository_id, version_identity, version_date) VALUES(@repository_id, @version_identity, @version_date)";
+            List<SqlParameter> sqlParameters = new List<SqlParameter>();
+            sqlParameters.Add(new SqlParameter("@repository_id", version.version_id));
+            sqlParameters.Add(new SqlParameter("@version_identity", version.version_identity));
+            sqlParameters.Add(new SqlParameter("@version_date", version.version_date));
+            return update(dataBase, sql, sqlParameters);
+        }
+
+    }
+}
---
+++ CGit/Src/Util/EmailUtil.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+
+namespace CGit.Src.Util
+{
+    public static class EmailUtil
+    {
+        /// <summary>
+        /// 发送邮件功能
+        /// </summary>
+        /// <param name="to">收件人</param>
+        /// <param name="body">正文</param>
+        /// <param name="title">标题</param>
+        /// <param name="Fname">发件人姓名</param>
+        /// <returns></returns>
+        public static bool SentMail(string to, string body, string title, string Fname)
+        {
+            bool retrunBool = false;
+            MailMessage mail = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+            string strFromEmail = "1137889088@qq.com";//你的邮箱
+            string strEmailPassword = "ylmqrxctxycybagj";//QQPOP3/SMTP服务码
+            try
+            {
+                mail.From = new MailAddress("" + Fname + "<" + strFromEmail + ">");
+                mail.To.Add(new MailAddress(to));
+                mail.BodyEncoding = Encoding.UTF8;
+                mail.IsBodyHtml = true;
+                mail.SubjectEncoding = Encoding.UTF8;
+                mail.Priority = MailPriority.Normal;
+                mail.Body = body;//正文
+                mail.Subject = title;//标题
+                smtp.EnableSsl = true;//不设置    QQ发送会出错
+                smtp.Host = "smtp.qq.com";//可以换成163
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Credentials = new System.Net.NetworkCredential(strFromEmail, strEmailPassword);
+                //发送邮件
+                try
+                {
+                    smtp.Send(mail);   //同步发送
+                }
+                catch (Exception ex)
+                {
+                   
+                }
+                retrunBool = true;
+            }
+            catch (Exception ex)
+            {
+                retrunBool = false;
+            }
+            //smtp.SendAsync(mail, mail.To); //异步发送 （异步发送时页面上要加上Async="true" ）
+            return retrunBool;
+        }
+    }
+}
